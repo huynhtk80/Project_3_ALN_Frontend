cb953dfe42e5178f58a8f7192abdb649c364a000
Timothy H <huynh.tk80@gmail.com>
2022-12-15 11:49:09 -0700

    Basic Nav Layout and Routes Setup

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Landing() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>Landing</h1>
+      <Link to="/home">Get Started</Link>
+    </>
+  );
+}
+
+export default Landing;
