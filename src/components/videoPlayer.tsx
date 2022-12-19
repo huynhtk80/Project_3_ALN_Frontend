129333201bde2acc2fe5c7fb1aaa4a7d0fcceeb0
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2022-12-19 16:45:21 -0700

    Added working video player without controls

---
+++ src/components/videoPlayer.tsx
@@ -0,0 +1,18 @@
+import './videoPlayer.css'
+import ReactPlayer from 'react-player'
+import React from 'react'
+import { SourceProps } from 'react-player/base'
+
+function VideoPlayer(props: { url: string | string[] | SourceProps[] | MediaStream | undefined }) {
+  return (
+    <>
+    <div className="videoPlayer">
+      <ReactPlayer url={props.url}/>
+      
+    </div>
+    
+</>
+  )
+}
+
+export default VideoPlayer
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import VideoPlayer from "../components/videoPlayer";
+
+
+
 
 function Home() {
-  return <div>Home</div>;
+  return <>
+  <VideoPlayer url="https://www.youtube.com/watch?v=yVQB1TVcD2k"/>;
+  <VideoPlayer url="https://dash.akamaized.net/dash264/TestCasesHD/2b/qualcomm/1/MultiResMPEG2.mpd"/>
+  </>
 }
 
 export default Home;
