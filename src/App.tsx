738ecb015522b2327035971764ce650cade2b7fa
Timothy H <huynh.tk80@gmail.com>
2022-12-19 13:28:45 -0700

    playing with aI nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Navbar from "./components/Navtest";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Layout />}>
+        <Route path="/home" element={<Navbar />}>
           <Route index element={<Home />} />
           <Route path="Profile" element={<Profile />} />
         </Route>
