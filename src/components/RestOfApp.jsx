1993950db26fcb293fcd29d7bf5d60c82293351b
Timothy H <huynh.tk80@gmail.com>
2022-12-21 14:23:27 -0700

    updated routes to NewBar

--- src/components/RestOfApp.jsx
+++ src/components/RestOfApp.jsx
@@ -1,35 +1,34 @@
-import React, {useContext } from 'react'
+import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Profile from "../pages/Profile";
-import Layout from "../components/Layout";
 import Home from "../pages/Home";
-import { FirebaseContext } from '../providers/FirebaseProvider';
+import { FirebaseContext } from "../providers/FirebaseProvider";
+import Navbar from "./Navbar";
 
 export const RestOfApp = () => {
-    const fbContext = useContext(FirebaseContext);
-    const app = fbContext.app;
+  const fbContext = useContext(FirebaseContext);
+  const app = fbContext.app;
   return (
-    <div className='App'>
-        Firebase app info:<br/><br/>{JSON.stringify(fbContext.app)}
-        </div>
+    <div className="App">
+      Firebase app info:
+      <br />
+      <br />
+      {JSON.stringify(fbContext.app)}
+    </div>
   );
 
-  
-function App() {
+  function App() {
     return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/home" element={<Layout />}> 
-        <Route index element={<Home />} />
-        <Route path="Profile" element={<Profile />} />
-    </Route>
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="Profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     );
-
-  
-}
-
-}
+  }
+};
