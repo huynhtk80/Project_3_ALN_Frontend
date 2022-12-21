8da47294481d4b98181b79b499f5949c4bfb9061
Timothy H <huynh.tk80@gmail.com>
2022-12-21 15:10:02 -0700

    Fix routes

--- src/components/RestOfApp.jsx
+++ src/components/RestOfApp.jsx
@@ -5,30 +5,25 @@ import Profile from "../pages/Profile";
 import Home from "../pages/Home";
 import { FirebaseContext } from "../providers/FirebaseProvider";
 import Navbar from "./Navbar";
+import Network from "../pages/Network";
+import Videos from "../pages/Videos";
+import Settings from "../pages/Settings";
 
 export const RestOfApp = () => {
   const fbContext = useContext(FirebaseContext);
   const app = fbContext.app;
   return (
-    <div className="App">
-      Firebase app info:
-      <br />
-      <br />
-      {JSON.stringify(fbContext.app)}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="Profile" element={<Profile />} />
+          <Route path="network" element={<Network />} />
+          <Route path="videos" element={<Videos />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-
-  function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Navbar />}>
-            <Route index element={<Home />} />
-            <Route path="Profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
 };
