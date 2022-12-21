47abf160be9f2a2675cc19906dceb8088e8f68cd
Timothy H <huynh.tk80@gmail.com>
2022-12-21 15:39:50 -0700

    Revert "Clean up navbar css"
    This reverts commit 470b2793a07a35e5123030bfb7e94d93c785afa3.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,8 @@
-const plugin = require("tailwindcss/plugin");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    plugin(function ({ addVariant }) {
-      addVariant("current", "&.active");
-    }),
-  ],
+  plugins: [],
 };
