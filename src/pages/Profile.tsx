1197cc03809a679d30707b6b0cfbf3ccb5e95916
Timothy H <huynh.tk80@gmail.com>
2022-12-22 09:01:44 -0700

    temp added Firebase app info

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { FirebaseContext } from "../providers/FirebaseProvider";
 
 function Profile() {
+  const fbContext = useContext(FirebaseContext);
+  const app = fbContext.app;
   return (
     <div>
       <h1>My Profile</h1>
+      <div className="App">
+        Firebase app info:
+        <br />
+        <br />
+        {JSON.stringify(fbContext.app)}
+      </div>
     </div>
   );
 }
