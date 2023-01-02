65df25b18bd9ce9416b1c15887e10e8f70f60554
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2023-01-02 16:31:50 -0700

    flxed login form borders fixed login button layout

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,33 +1,45 @@
-import React, { useContext, useState } from "react";
-import { AuthContext } from "../providers/AuthProvider";
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
 
 export const LoginForm = () => {
   const authContext = useContext(AuthContext);
   const loginFn = authContext.login;
   const logoutFn = authContext.logout;
   const user = authContext.user;
-  const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
   return (
-    <div>
+    <div className=''>
       {!user ? (
         <>
-      <input
-        name="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-      <input
-        name="password"
-        value={password}
-        onChange={(e) => setpassword(e.target.value)}
-      />
-      <br />
-      <button onClick={() => loginFn(email, password)}>LOGIN</button>
-      </>
+          <input
+            className='border border-black rounded m-1 p-1'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            className='border border-black rounded m-1 p-1'
+            name='password'
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+          />
+          <br />
+          <button
+            className='bg-sky-700 hover:bg-sky-900 text-white font-bold py-2 px-4 rounded'
+            onClick={() => loginFn(email, password)}
+          >
+            LOGIN
+          </button>
+        </>
       ) : (
-     <button onClick={() => logoutFn()}>LOG OUT</button>
+        <button
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          onClick={() => logoutFn()}
+        >
+          LOG OUT
+        </button>
       )}
     </div>
   );
--- src/components/RestOfApp.jsx
+++ src/components/RestOfApp.jsx
@@ -1,45 +1,40 @@
-import React, { useContext } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "../pages/Landing";
-import Profile from "../pages/Profile";
-import Home from "../pages/Home";
-import { FirebaseContext } from "../providers/FirebaseProvider";
-import Navbar from "./Navbar";
-import Network from "../pages/Network";
-import Videos from "../pages/Videos";
-import Settings from "../pages/Settings";
-import Logout from "../pages/Logout";
-import NotFound from "../pages/NotFound";
-import { AuthContext } from "../providers/AuthProvider";
-import { LoginForm } from "./LoginForm";
+import React, { useContext } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from '../pages/Landing';
+import Profile from '../pages/Profile';
+import Home from '../pages/Home';
+import { FirebaseContext } from '../providers/FirebaseProvider';
+import Navbar from './Navbar';
+import Network from '../pages/Network';
+import Videos from '../pages/Videos';
+import Settings from '../pages/Settings';
+import Logout from '../pages/Logout';
+import NotFound from '../pages/NotFound';
+import { AuthContext } from '../providers/AuthProvider';
+import { LoginForm } from './LoginForm';
 
 export const RestOfApp = () => {
   const fbContext = useContext(FirebaseContext);
   const app = fbContext.app;
   const authContext = useContext(AuthContext);
-  const user = authContext.user
-  
+  const user = authContext.user;
+
   return (
-   
-    
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Navbar />}>
-            <Route index element={<Home />} />
-            <Route path="Profile" element={<Profile />} />
-            <Route path="network" element={<Network />} />
-            <Route path="videos" element={<Videos />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="logout" element={<Logout />} />
-            <Route path="LoginForm" element={<LoginForm />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/home' element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path='Profile' element={<Profile />} />
+          <Route path='network' element={<Network />} />
+          <Route path='videos' element={<Videos />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='logout' element={<Logout />} />
+          <Route path='LoginForm' element={<LoginForm />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
-
-
