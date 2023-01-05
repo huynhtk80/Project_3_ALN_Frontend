e1e2c251b4ea144a2840d7116000fed0cc6fe24d
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2023-01-05 03:46:04 -0700

    added dark mode toggle, functionality needs work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,27 +4,29 @@ import VideoPlayer from '../components/videoPlayer';
 function Home() {
   return (
     <>
-      <h1 className='text-center text-cyan-900 tracking-wide text-3xl'>
-        Africa Live Network Videos
-      </h1>
-      <div className='grid grid-cols-2 grid-rows-3 gap-10 p-5 justify-items-center'>
-        <div>
-          <VideoPlayer url='https://www.youtube.com/watch?v=vbmfVubluMc' />
-        </div>
-        <div>
-          <VideoPlayer url='https://www.youtube.com/watch?v=34-5ZRq3W_M' />
-        </div>
-        <div>
-          <VideoPlayer url='https://www.youtube.com/watch?v=p00MGPjp1Ow' />
-        </div>
-        <div>
-          <VideoPlayer url='https://www.youtube.com/watch?v=g6JGg16O-ig' />
-        </div>
-        <div>
-          <VideoPlayer url='https://www.youtube.com/watch?v=3h0cw6EOnFw' />
-        </div>
-        <div>
-          <VideoPlayer url='https://www.youtube.com/watch?v=WQQKkjmc8wo' />
+      <div className='bg-white dark:bg-grey-800 '>
+        <h1 className='text-center text-cyan-900 tracking-wide text-3xl'>
+          Africa Live Network Videos
+        </h1>
+        <div className='grid grid-cols-2 grid-rows-3 gap-10 p-5 justify-items-center'>
+          <div>
+            <VideoPlayer url='https://www.youtube.com/watch?v=vbmfVubluMc' />
+          </div>
+          <div>
+            <VideoPlayer url='https://www.youtube.com/watch?v=34-5ZRq3W_M' />
+          </div>
+          <div>
+            <VideoPlayer url='https://www.youtube.com/watch?v=p00MGPjp1Ow' />
+          </div>
+          <div>
+            <VideoPlayer url='https://www.youtube.com/watch?v=g6JGg16O-ig' />
+          </div>
+          <div>
+            <VideoPlayer url='https://www.youtube.com/watch?v=3h0cw6EOnFw' />
+          </div>
+          <div>
+            <VideoPlayer url='https://www.youtube.com/watch?v=WQQKkjmc8wo' />
+          </div>
         </div>
       </div>
     </>
