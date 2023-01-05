ab268a75d039180b27553799206e80a68f3136b9
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2023-01-04 23:32:44 -0700

    updated FirebaseProvider with cloud storage created FetchData.tsx to fetch videos from cloud

--- src/providers/FirebaseProvider.jsx
+++ src/providers/FirebaseProvider.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+import React from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqPeb96DfaW9VM1tQK68Y3rqwCDTfyNZo",
-  authDomain: "project3-aln.firebaseapp.com",
-  projectId: "project3-aln",
-  storageBucket: "project3-aln.appspot.com",
-  messagingSenderId: "262376267458",
-  appId: "1:262376267458:web:28d298323a1e0e23b7255b"
+  apiKey: 'AIzaSyAqPeb96DfaW9VM1tQK68Y3rqwCDTfyNZo',
+  authDomain: 'project3-aln.firebaseapp.com',
+  projectId: 'project3-aln',
+  storageBucket: 'project3-aln.appspot.com',
+  messagingSenderId: '262376267458',
+  appId: '1:262376267458:web:28d298323a1e0e23b7255b',
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
 export const FirebaseContext = React.createContext();
 
-
 export const FirebaseProvider = (props) => {
-    const { children } = props;
+  const { children } = props;
 
-    const theValues = { app, auth };
-    return (
-     <FirebaseContext.Provider value={theValues}>
-       {children}
-     </FirebaseContext.Provider>
-    );
+  const theValues = { app, auth };
+  return (
+    <FirebaseContext.Provider value={theValues}>
+      {children}
+    </FirebaseContext.Provider>
+  );
 };
---
+++ src/utils/FetchData.tsx
@@ -0,0 +1,76 @@
+import {
+  collection,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  where,
+  doc,
+  deleteDoc,
+} from 'firebase/firestore';
+import { FirebaseProvider } from '../providers/FirebaseProvider';
+
+export const getAllVideos = async (fireStoreDb) => {
+  const feeds = await getDocs(
+    query(collection(fireStoreDb, 'videos'), orderBy('id', 'desc'))
+  );
+  return feeds.docs.map((doc) => doc.data());
+};
+
+export const categoryFeeds = async (fireStoreDb, categoryId) => {
+  const feeds = await getDocs(
+    query(
+      collection(fireStoreDb, 'videos'),
+      where('category', '==', categoryId),
+      orderBy('id', 'desc')
+    )
+  );
+  return feeds.docs.map((doc) => doc.data());
+};
+
+export const recommendedFeed = async (fireStoreDb, categoryId, videoId) => {
+  const feeds = await getDocs(
+    query(
+      collection(fireStoreDb, 'videos'),
+      where('category', '==', categoryId),
+      where('id', '!=', videoId),
+      orderBy('id', 'desc')
+    )
+  );
+  return feeds.docs.map((doc) => doc.data());
+};
+
+export const getSpecificVideo = async (fireStoreDb, videoId) => {
+  const videoRef = doc(fireStoreDb, 'videos', videoId);
+  const videoSnap = await getDoc(videoRef);
+  if (videoSnap.exists()) {
+    return videoSnap.data();
+  } else {
+    return 'No such document';
+  }
+};
+
+export const getUserInfo = async (fireStoreDb, userId) => {
+  const userRef = doc(fireStoreDb, 'users', userId);
+  const userSnap = await getDoc(userRef);
+  if (userSnap.exists()) {
+    return userSnap.data();
+  } else {
+    return 'No such Document';
+  }
+};
+
+export const userCreatedVideos = async (fireStoreDb, userId) => {
+  const feeds = await getDocs(
+    query(
+      collection(fireStoreDb, 'videos'),
+      where('userId', '==', userId),
+      orderBy('id', 'desc')
+    )
+  );
+  return feeds.docs.map((doc) => doc.data());
+};
+
+export const deleteVideo = async (fireStoreDb, videoId) => {
+  await deleteDoc(doc(fireStoreDb, 'videos', videoId));
+};
