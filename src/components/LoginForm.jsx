afb3673404845f7468e4fccdcee422577b66ef38
Chelsea Mansoff <chelyrose888@gmail.com>
2023-01-06 10:29:48 -0700

    added preventDefault in LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -89,7 +89,11 @@ export const LoginForm = () => {
               <button
                 type='submit'
                 className='group relative flex w-full justify-center rounded-md border border-transparent bg-blue-500 py-2 px-4 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                onClick={() => loginFn(email, password)}
+                
+                onClick={(e) => {
+                  e.preventDefault();
+                  loginFn(email, password);
+                }}
               >
                 <span className='absolute inset-y-0 left-0 flex items-center pl-3'></span>
                 Sign in
