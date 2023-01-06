2073117c5f1634e23528b01b68af18c2235708d1
Timothy H <huynh.tk80@gmail.com>
2023-01-06 11:31:45 -0700

    Created Mock Db

---
+++ src/utils/mockDb.tsx
@@ -0,0 +1,34 @@
+export const videoArray = [
+  {
+    title: "Jamaican 60th Celebration",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    url: "https://www.youtube.com/watch?v=vbmfVubluMc",
+    thumbnailUrl: "https://picsum.photos/500/300",
+    categories: "Documentary",
+  },
+  {
+    title: "Cleaning for Hope",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    url: "https://www.youtube.com/watch?v=34-5ZRq3W_M",
+    thumbnailUrl: "https://picsum.photos/500/300",
+    categories: "Documentary",
+  },
+  {
+    title: "Nu G Ep4 - Candice",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    url: "https://www.youtube.com/watch?v=34-5ZRq3W_M",
+    thumbnailUrl: "https://picsum.photos/500/300",
+    categories: "Documentary",
+  },
+  {
+    title: "JPEC Documentary University of Iowa",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    url: "https://www.youtube.com/watch?v=g6JGg16O-ig",
+    thumbnailUrl: "https://picsum.photos/500/300",
+    categories: "Documentary",
+  },
+];
