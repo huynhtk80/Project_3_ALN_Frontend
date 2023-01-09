a60bb45435b8628e2670888a35d13ad8f2f7bb29
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-01-09 12:36:05 -0700

    worked on video 🍽

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import VideoThumbCard from "../components/VideoThumbCard";
 import { videoArray } from "../utils/mockDb";
 
 
 function Videos() {
+  const [activeIndex, setActiveIndex] = useState<number|null>(null); 
+  
   return (
     <div>
       <h1>My Videos</h1>
       <div className="flex flex-row flex-wrap justify-evenly" >
 
-      {videoArray.map((vid)=>(
-        <VideoThumbCard url={vid.url} description={vid.description} title={vid.title}/>
-      ))}
+      {videoArray.map((vid, key)=>{
+        console.log(vid)
+        console.log(key)
+        return(
+        <VideoThumbCard url={vid.url} description={vid.description} title={vid.title} key={key} activeIndex={activeIndex} setActiveIndex={setActiveIndex}/>
+      )}
+      )}
       
       </div>
     </div>
