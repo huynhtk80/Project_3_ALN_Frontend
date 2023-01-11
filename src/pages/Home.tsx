7f0021b253ff37df3da0c7dcc8d3d2c8da51ee1a
Timothy H <huynh.tk80@gmail.com>
2023-01-11 00:30:00 -0700

    arrows added back to carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,34 +19,48 @@ function Home() {
         <div id='slide1' className='carousel-item relative w-full'>
           <img src='https://placeimg.com/800/200/arch' className='w-full' />
           <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-            <a href='#slide4' className='btn btn-circle'></a>
-            <a href='#slide2' className='btn btn-circle'></a>
+            <a href='#slide4' className='btn btn-circle'>
+              ❮
+            </a>
+            <a href='#slide2' className='btn btn-circle'>
+              ❯
+            </a>
           </div>
         </div>
         <div id='slide2' className='carousel-item relative w-full'>
           <img src='https://placeimg.com/800/200/nature' className='w-full' />
           <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-            <a href='#slide1' className='btn btn-circle'></a>
-            <a href='#slide3' className='btn btn-circle'></a>
+            <a href='#slide1' className='btn btn-circle'>
+              ❮
+            </a>
+            <a href='#slide3' className='btn btn-circle'>
+              ❯
+            </a>
           </div>
         </div>
         <div id='slide3' className='carousel-item relative w-full'>
           <img src='https://placeimg.com/800/200/tech' className='w-full' />
           <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-            <a href='#slide2' className='btn btn-circle'></a>
-            <a href='#slide4' className='btn btn-circle'></a>
+            <a href='#slide2' className='btn btn-circle'>
+              ❮
+            </a>
+            <a href='#slide4' className='btn btn-circle'>
+              ❯
+            </a>
           </div>
         </div>
         <div id='slide4' className='carousel-item relative w-full'>
           <img src='https://placeimg.com/800/200/any' className='w-full' />
           <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
             <a href='#slide3' className='btn btn-circle'></a>
-            <a href='#slide1' className='btn btn-circle'></a>
+            <a href='#slide1' className='btn btn-circle'>
+              ❯
+            </a>
           </div>
         </div>
       </div>
 
-      <div className='flex flex-row justify-center gap-4 my-5'>
+      <div className='flex flex-row flex-wrap justify-center gap-4 my-5'>
         {videoArray.map((vid) => (
           <div className='card w-96 bg-base-100 shadow-xl image-full'>
             <figure>
