460805f41c35030af3ec3e4c3966c5fe39fc4b52
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-01-10 21:44:19 -0700

    Fixed player issue** Player is now capable of stopping one video when starting the next without user manual imput. **Updated key name to Index to resolve above issue. **Changed Header Title on Videos page**

--- src/components/VideoThumbCard.tsx
+++ src/components/VideoThumbCard.tsx
@@ -7,18 +7,18 @@ interface AppProps {
   url: string
   title: string
   description: string
-  key: number
+  index: number
   activeIndex: number|null
   setActiveIndex?: any
 }
 
-function VideoThumbCard({url, title, description, key, activeIndex, setActiveIndex}: AppProps) {
-  console.log(title, activeIndex, key)
+function VideoThumbCard({url, title, description, index: index, activeIndex, setActiveIndex}: AppProps) {
+  console.log(title, activeIndex, index)
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <div className= "flex justify-center">
       <ReactPlayer
-              id={key} height="215px" url={url} playing={activeIndex === key  ? true : false} onPlay={()=>{setActiveIndex(key)}}
+              id={index} height="215px" url={url} playing={activeIndex === index  ? true : false} onPlay={()=>{setActiveIndex(index)}}
       /></div>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
