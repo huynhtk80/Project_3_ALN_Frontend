6f52870d959db38d8eb8ded558d8512d3fd38f96
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-01-17 16:27:20 -0700

    *Added Map to Home page, *Corrected OnClick issue and got map working when clicking onthe country

--- src/components/VideoThumbCard.tsx
+++ src/components/VideoThumbCard.tsx
@@ -27,6 +27,50 @@ function VideoThumbCard({url, title, description, index: index, activeIndex, set
       </div>
          </div>
   );
+
+  <div className='flex flex-row flex-wrap justify-center gap-4 my-5'>
+        {VideoPlayer.map((vid) => (
+          <div className='card w-96 bg-base-100 shadow-xl image-full'>
+            <figure>
+              {/* <ReactPlayer url={vid.url} height='225px' /> */}
+              <img src={vid.thumbnailUrl} alt='Shoes' />
+            </figure>
+            <div className='card-body'>
+              <h2 className='card-title'>{vid.title}</h2>
+              <p>{vid.description}</p>
+              <div className='card-actions justify-end'>
+                <div className='stats shadow'>
+                  <div className='stat'>
+                    <div className='stat-figure text-primary'></div>
+                    <div className='stat-title'>Total Likes</div>
+                    <div className='stat-value text-primary'>25.6K</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <div className='grid grid-cols-2 grid-rows-3 gap-10 p-5 justify-items-center'>
+        <div>
+          <VideoPlayer url='https://www.youtube.com/watch?v=vbmfVubluMc' />
+        </div>
+        <div>
+          <VideoPlayer url='https://www.youtube.com/watch?v=34-5ZRq3W_M' />
+        </div>
+        <div>
+          <VideoPlayer url='https://www.youtube.com/watch?v=p00MGPjp1Ow' />
+        </div>
+        <div>
+          <VideoPlayer url='https://www.youtube.com/watch?v=g6JGg16O-ig' />
+        </div>
+        <div>
+          <VideoPlayer url='https://www.youtube.com/watch?v=3h0cw6EOnFw' />
+        </div>
+        <div>
+          <VideoPlayer url='https://www.youtube.com/watch?v=WQQKkjmc8wo' />
+        </div>
+      </div> */}
 }
 
 export default VideoThumbCard;
