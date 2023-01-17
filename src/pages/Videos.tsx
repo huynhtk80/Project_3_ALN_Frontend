238b3a37ecaa2134c08f848f78ba29ad7ada29f2
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2023-01-17 11:01:18 -0700

    made changes to the videos pages  to have gap in between

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
-import VideoThumbCard from "../components/VideoThumbCard";
-import { videoArray } from "../utils/mockDb";
-
+import React, { useState } from 'react';
+import VideoThumbCard from '../components/VideoThumbCard';
+import { videoArray } from '../utils/mockDb';
 
 function Videos() {
-  const [activeIndex, setActiveIndex] = useState<number|null>(null); 
-  
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
   return (
     <div>
       <h1>Africa Live Network - Under Construction - Beta Testing Site </h1>
-      <div className="flex flex-row flex-wrap justify-evenly" >
-
-      {videoArray.map((vid, index)=>{
-        console.log(vid)
-        console.log(index)
-        return(
-        <VideoThumbCard url={vid.url} description={vid.description} title={vid.title} index={index} activeIndex={activeIndex} setActiveIndex={setActiveIndex}/>
-      )}
-      )}
-      
-      
+      <div className='flex flex-row flex-wrap justify-evenly mt-10 gap-10'>
+        {videoArray.map((vid, index) => {
+          console.log(vid);
+          console.log(index);
+          return (
+            <VideoThumbCard
+              url={vid.url}
+              description={vid.description}
+              title={vid.title}
+              index={index}
+              activeIndex={activeIndex}
+              setActiveIndex={setActiveIndex}
+            />
+          );
+        })}
       </div>
     </div>
   );
