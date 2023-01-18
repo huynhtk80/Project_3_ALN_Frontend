02113ed63028664f16ac9100498261001dfc7c3b
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-01-17 21:17:24 -0700

    Changed Text under Network

--- src/pages/Network.tsx
+++ src/pages/Network.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 function Network() {
   return (
     <div>
-      <h1>My Network - Coming Soon - Africa Live Network Under Construction - Beta Testing Site</h1>
+      <h1>
+        Future African Business Network - Coming Soon - Africa Live Network
+        Under Construction - Beta Testing Site
+      </h1>
     </div>
   );
 }
