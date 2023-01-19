33e5ecc2361772ba54807692a14fd637b0befca4
Timothy H <huynh.tk80@gmail.com>
2023-01-19 10:46:48 -0700

    Template: pull params

--- src/pages/Podcasts.tsx
+++ src/pages/Podcasts.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+import { FirebaseContext } from '../providers/FirebaseProvider';
+import { VideoParams } from '../utils/FireStoreAPI';
 
 function Podcasts() {
+  const { category } = useParams();
+  const fbContext = useContext(FirebaseContext);
+  const { user } = useContext(AuthContext);
+  const db = fbContext.db;
+  const [videos, setVideos] = useState<VideoParams[]>([
+    {
+      userId: '',
+      title: '',
+      url: '',
+      videoFileId: '',
+      thumbnail: '',
+      thumbnailFileId: '',
+      description: '',
+      collection: '',
+      DOC_ID: '',
+      approval: '',
+      rejectMsg: '',
+    },
+  ]);
+
+  console.log(category);
+
+  useEffect(() => {
+    if (!user) return;
+    let collectionRef = collection(db, 'videos');
+
+    let queryRef = query(collectionRef, where('collection', '==', category));
+    const unsubscribe = onSnapshot(queryRef, (querySnap) => {
+      if (querySnap.empty) {
+        console.log('No docs found');
+        setVideos([]);
+      } else {
+        let videoData = querySnap.docs.map(
+          (doc) =>
+            ({
+              ...doc.data(),
+              DOC_ID: doc.id,
+            } as VideoParams)
+        );
+        setVideos(videoData);
+      }
+    });
+    return unsubscribe;
+  }, [user]);
+
   return (
     <div>
       <h1>
         Coming Soon - Africa Live Network Under Construction - Beta Testing Site
       </h1>
+      <p>{category}</p>
+      {videos?.map((vid) => {
+        return <video controls poster={vid.thumbnail} src={vid.url}></video>;
+      })}
     </div>
   );
 }
