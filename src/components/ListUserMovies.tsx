28fb06dac87a1e4c9cd01a45b3f62887c43ae567
Timothy H <huynh.tk80@gmail.com>
2023-01-21 08:59:25 -0700

    Approval Status moved to own component

--- src/components/ListUserMovies.tsx
+++ src/components/ListUserMovies.tsx
@@ -15,6 +15,7 @@ import { AuthContext } from '../providers/AuthProvider';
 
 import UploadedVidDetail from './UploadedVidDetail';
 import { VideoParams } from '../utils/fireStoreAPI';
+import ApprovalStatus from './ApprovalStatus';
 
 function ListUserMovies() {
   const fbContext = useContext(FirebaseContext);
@@ -81,7 +82,7 @@ function ListUserMovies() {
     }
   };
 
-  const handleCheckClick = (e: any) => {
+  const handleCheckClick = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, checked } = e.target;
     setIsCheck([...isCheck, id]);
     if (!checked) {
@@ -106,95 +107,6 @@ function ListUserMovies() {
     });
   };
 
-  const onClickApproveHandle = async (
-    videoId: string,
-    action: 'cancel' | 'submit'
-  ) => {
-    const docRef = doc(db, 'videos', videoId);
-    if (action === 'submit') {
-      await updateDoc(docRef, {
-        approval: 'pending',
-      });
-    } else {
-      await updateDoc(docRef, {
-        approval: '',
-      });
-    }
-  };
-
-  const renderApprovalStatus = (approval: string, viddocId: string) => {
-    if (approval == 'pending') {
-      return (
-        <>
-          <p>pending</p>
-          <button
-            className='btn btn-primary btn-sm'
-            onClick={() => onClickApproveHandle(viddocId, 'cancel')}
-          >
-            Cancel
-          </button>
-        </>
-      );
-    } else if (approval === 'approved') {
-      return <p>Approved</p>;
-    } else if (approval === 'reject') {
-      return (
-        <>
-          Rejected
-          <div className=' inline-block dropdown dropdown-end'>
-            <label
-              tabIndex={0}
-              className='btn btn-circle btn-ghost btn-xs text-info'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                className='w-4 h-4 stroke-current'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                ></path>
-              </svg>
-            </label>
-            <div
-              tabIndex={0}
-              className='card compact dropdown-content shadow bg-base-100 rounded-box w-64'
-            >
-              <div className='card-body'>
-                <h2 className='card-title'>Reason for Rejection</h2>
-                <p>
-                  {videos.find((vid) => vid.DOC_ID === viddocId)?.rejectMsg}
-                </p>
-              </div>
-            </div>
-          </div>
-          <br />
-          <button
-            className='btn btn-primary btn-sm'
-            onClick={() => onClickApproveHandle(viddocId, 'submit')}
-          >
-            Resubmit for <br />
-            Approval
-          </button>
-        </>
-      );
-    } else {
-      return (
-        <button
-          className='btn btn-primary btn-sm'
-          onClick={() => onClickApproveHandle(viddocId, 'submit')}
-        >
-          Submit for <br />
-          Approval
-        </button>
-      );
-    }
-  };
-
   return (
     <>
       <div className='text-center text-cyan-900 tracking-wide text-3xl mt-6'>
@@ -217,7 +129,6 @@ function ListUserMovies() {
               </th>
               <th>Video</th>
               <th>Title</th>
-              <th>Description</th>
               <th>Collection</th>
               <th>Details</th>
               <th>approval</th>
@@ -286,9 +197,6 @@ function ListUserMovies() {
                     />
                   </td>
                   <td>{video.title}</td>
-                  <td className='min-w-[12rem] max-w-[20rem] whitespace-normal'>
-                    {video.description}
-                  </td>
                   <td>{video.collection}</td>
                   <th>
                     <button
@@ -298,7 +206,9 @@ function ListUserMovies() {
                       details
                     </button>
                   </th>
-                  <th>{renderApprovalStatus(video.approval, video.DOC_ID)}</th>
+                  <th>
+                    <ApprovalStatus video={video} isSaved={true} />
+                  </th>
                 </tr>
               );
             })}
@@ -309,7 +219,6 @@ function ListUserMovies() {
               <th></th>
               <th>Video</th>
               <th>Title</th>
-              <th>Description</th>
               <th>Collection</th>
               <th>Details</th>
               <th>Approval</th>
