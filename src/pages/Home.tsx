bcace966f64261f866bb2891f5573f6a39f22cc3
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-01-21 18:14:12 -0700

    Fixed Home Errors that were preventing rendering.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,59 +7,61 @@ import AfricanMap from '../components/AfricanMap';
 function Home() {
   return (
     <>
-      {/* <div className='bg-base-100 '>
-        <div className='carousel w-full'>
-          <div id='slide1' className='carousel-item relative w-full'>
-            <img src='https://placeimg.com/800/200/arch' className='w-full' />
-            <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-              <a href='#slide4' className='btn btn-circle'>
-                ❮
-              </a>
-              <a href='#slide2' className='btn btn-circle'>
-                ❯
-              </a>
+      {
+        <div className='bg-base-100 '>
+          <div className='carousel w-full'>
+            <div id='slide1' className='carousel-item relative w-full'>
+              <img src='https://placeimg.com/800/200/arch' className='w-full' />
+              <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+                <a href='#slide4' className='btn btn-circle'>
+                  ❮
+                </a>
+                <a href='#slide2' className='btn btn-circle'>
+                  ❯
+                </a>
+              </div>
             </div>
-          </div>
-          //* TODO: Add Top Videos / Eventually to be Recommended Videos*//
-          <div id='slide2' className='carousel-item relative w-full'>
-            <img src='https://placeimg.com/800/200/nature' className='w-full' />
-            <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-              <a href='#slide1' className='btn btn-circle'>
-                ❮
-              </a>
-              <a href='#slide3' className='btn btn-circle'>
-                ❯
-              </a>
+            <div id='slide2' className='carousel-item relative w-full'>
+              <img
+                src='https://placeimg.com/800/200/nature'
+                className='w-full'
+              />
+              <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+                <a href='#slide1' className='btn btn-circle'>
+                  ❮
+                </a>
+                <a href='#slide3' className='btn btn-circle'>
+                  ❯
+                </a>
+              </div>
             </div>
-          </div>
-          //* TODO: Add Top Videos / Eventually to be Recommended Videos*//
-          <div id='slide3' className='carousel-item relative w-full'>
-            <img src='https://placeimg.com/800/200/tech' className='w-full' />
-            <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-              <a href='#slide2' className='btn btn-circle'>
-                ❮
-              </a>
-              <a href='#slide4' className='btn btn-circle'>
-                ❯
-              </a>
+            <div id='slide3' className='carousel-item relative w-full'>
+              <img src='https://placeimg.com/800/200/tech' className='w-full' />
+              <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+                <a href='#slide2' className='btn btn-circle'>
+                  ❮
+                </a>
+                <a href='#slide4' className='btn btn-circle'>
+                  ❯
+                </a>
+              </div>
             </div>
-          </div>
-          //* TODO: Add Top Videos / Eventually to be Recommended Videos*//
-          <div id='slide4' className='carousel-item relative w-full'>
-            <img src='https://placeimg.com/800/200/any' className='w-full' />
-            <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-              <a href='#slide3' className='btn btn-circle'>
-                ❮
-              </a>
-              <a href='#slide1' className='btn btn-circle'>
-                ❯
-              </a>
+            <div id='slide4' className='carousel-item relative w-full'>
+              <img src='https://placeimg.com/800/200/any' className='w-full' />
+              <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+                <a href='#slide3' className='btn btn-circle'>
+                  ❮
+                </a>
+                <a href='#slide1' className='btn btn-circle'>
+                  ❯
+                </a>
+              </div>
             </div>
+            //* TODO: Add Top Videos / Eventually to be Recommended Videos*//
           </div>
-          //* TODO: Add Top Videos / Eventually to be Recommended Videos*//
         </div>
-      </div> */}
-      <AfricanMap />
+      }
+      <import AfricanMap />
     </>
   );
 }
