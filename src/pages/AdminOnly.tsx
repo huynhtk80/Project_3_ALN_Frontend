f2ce95076fef3e6448d3327a338936f53ecf562e
Timothy H <huynh.tk80@gmail.com>
2023-02-02 14:13:26 -0700

    creator claims added

--- src/pages/AdminOnly.tsx
+++ src/pages/AdminOnly.tsx
@@ -10,17 +10,35 @@ function AdminOnly() {
   const { functions } = fbcontext;
 
   const addAdmin = httpsCallable(functions, 'addAdminRole');
+  const addCreator = httpsCallable(functions, 'addCreatorRole');
+  const deleteCreator = httpsCallable(functions, 'deleteCreatorRole');
 
   const onClickHandle = async () => {
     const result = await addAdmin({ email: user.email });
     console.log(result);
   };
 
+  const onClickHandleCreator = async () => {
+    const result = await addCreator({ email: user.email });
+    console.log(result);
+  };
+
+  const onClickHandleDelCreator = async () => {
+    const result = await deleteCreator({ email: user.email });
+    console.log(result);
+  };
+
   return (
     <>
-      <div>AdminOnly</div>
-      <button className='btn btn-primary' onClick={onClickHandle}>
+      <div className='pt-20'>AdminOnly</div>
+      <button className='btn btn-primary glass' onClick={onClickHandle}>
         add me as a admin
+      </button>
+      <button className='btn btn-primary' onClick={onClickHandleCreator}>
+        add me as a content creator
+      </button>
+      <button className='btn btn-primary' onClick={onClickHandleDelCreator}>
+        delete me as a content creator
       </button>
     </>
   );
