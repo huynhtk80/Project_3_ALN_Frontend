f847a2a468061f84cdc388a3ce6f682fb2ebaac1
Timothy H <huynh.tk80@gmail.com>
2023-02-03 09:45:49 -0700

    added intro video

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -6,13 +6,30 @@ import { photoCrop } from '../utils/photoCrop';
 import { uploadFileStorage, deleteFileURL } from '../utils/fireStorageAPI';
 import { VideoParams } from '../utils/fireStoreAPI';
 
+export interface UserProfileProps {
+  about: string;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  country: string;
+  streetAddress: string;
+  city: string;
+  stateProvince: string;
+  zipPostal: string;
+  interests: string;
+  photo: string;
+  coverPhoto: string;
+  introVideo: string;
+  DOC_ID: string;
+}
+
 export default function userInfo() {
   const fbContext = useContext(FirebaseContext);
   const { user } = useContext(AuthContext);
   const db = fbContext.db;
   const store = fbContext.store;
 
-  const [userProfile, setUserProfile] = useState({
+  const [userProfile, setUserProfile] = useState<UserProfileProps>({
     about: '',
     firstName: '',
     lastName: '',
@@ -26,8 +43,8 @@ export default function userInfo() {
     photo: '',
     coverPhoto: '',
     introVideo: '',
+    DOC_ID: '',
   });
-  const [avatarURL, setAvatarURL] = useState(null);
 
   useEffect(() => {
     if (!user) return;
@@ -50,6 +67,7 @@ export default function userInfo() {
 
     return unsubscribe;
   }, [user]);
+  ``;
 
   const onChangeAvatar = async (e) => {
     let file = e.target.files[0];
@@ -62,7 +80,7 @@ export default function userInfo() {
       'crop'
     );
     console.log('the avatar cropped', imageUrl);
-    setAvatarURL(imageUrl);
+
     if (userProfile.photo) {
       deleteFileURL(store, userProfile.photo);
     }
@@ -78,7 +96,7 @@ export default function userInfo() {
     await updateDoc(docRef, { photo: downloadUrl });
   };
 
-  const onChangeCover = async (e) => {
+  const onChangeCover = async (e: any) => {
     let file = e.target.files[0];
     const url = URL.createObjectURL(file);
     const { imageUrl, imageFile } = await photoCrop(
@@ -104,6 +122,21 @@ export default function userInfo() {
     await updateDoc(docRef, { coverPhoto: downloadUrl });
   };
 
+  const onChangeVideo = async (e: any) => {
+    let file = e.target.files[0];
+
+    const downloadUrl = await uploadFileStorage(
+      store,
+      user.uid,
+      file,
+      'user',
+      'profile'
+    );
+    setUserProfile({ ...userProfile, introVideo: downloadUrl });
+    const docRef = doc(db, 'userInfo', user.uid);
+    await updateDoc(docRef, { introVideo: downloadUrl });
+  };
+
   const onChangeHandle = (e: any) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -111,7 +144,6 @@ export default function userInfo() {
     setUserProfile({ ...userProfile, [name]: value });
   };
 
-  console.log(userProfile);
   const onClickSaveHandle = async (e: any) => {
     e.preventDefault();
     const docRef = doc(db, 'userInfo', user.uid);
@@ -154,7 +186,7 @@ export default function userInfo() {
                         name='about'
                         rows={3}
                         className='mt-1 block w-full rounded-md border-accent placeholder-primary-content bg-slate-300 shadow-sm focus:border-neutral focus:ring-neutral sm:text-sm'
-                        placeholder=' you@example.com'
+                        placeholder='Tell us about yourself'
                         defaultValue={''}
                         onChange={onChangeHandle}
                       />
@@ -206,13 +238,13 @@ export default function userInfo() {
                     <label className='block text-sm font-medium text-base-content'>
                       Cover photo
                     </label>
-                    <div className='relative mt-1 aspect-[2/1] sm:aspect-[3/1] md:aspect-[4/1] w-full rounded-md border-2 border-dashed border-red-300 overflow-hidden'>
+                    <div className=' group relative mt-1 aspect-[2/1] sm:aspect-[3/1] md:aspect-[4/1] w-full rounded-md border-2 border-dashed border-neutral-content overflow-hidden'>
                       <img
-                        className=' absolute top-1/2 left-1/2 min-w-full min-h-full -translate-x-1/2 -translate-y-1/2 rounded-md z-0'
+                        className=' absolute top-1/2 left-1/2 min-w-full min-h-full -translate-x-1/2 -translate-y-1/2 rounded-md z-10'
                         src={userProfile.coverPhoto}
                       />
-                      <div className='absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-center z-[9] bg-slate-100 w-40 h-28 rounded-lg drop-shadow-md opacity-60'></div>
-                      <div className='absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-center z-10'>
+                      <div className=' absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-center z-0 group-hover:z-[11] bg-slate-100 w-40 h-28 rounded-lg drop-shadow-md opacity-60'></div>
+                      <div className='absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-center z-0  group-hover:z-[11]'>
                         <svg
                           className='mx-auto h-12 w-12 text-base-content opacity-100'
                           stroke='currentColor'
@@ -228,8 +260,11 @@ export default function userInfo() {
                           />
                         </svg>
                         <div className='flex text-sm text-base-content'>
-                          <label htmlFor='file-upload' className='badge'>
-                            <span>Upload a file</span>
+                          <label
+                            htmlFor='file-upload'
+                            className='badge cursor-pointer text-center'
+                          >
+                            <span className='mx-auto'>Upload a file</span>
                             <input
                               id='file-upload'
                               name='file-upload'
@@ -241,6 +276,54 @@ export default function userInfo() {
                         </div>
                         <p className='text-xs text-base-content'>
                           PNG, JPG, GIF up to 10MB
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <label className='block text-sm font-medium text-base-content'>
+                      Profile Video
+                    </label>
+                    <div className=' group relative mt-1 aspect-video w-full rounded-md border-2 border-dashed border-neutral-content overflow-hidden'>
+                      <video
+                        className=' absolute top-1/2 left-1/2 min-w-full min-h-full -translate-x-1/2 -translate-y-1/2 rounded-md z-10'
+                        src={userProfile.introVideo}
+                        controls
+                      />
+                      <div className=' absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-center z-0 group-hover:z-[11] bg-slate-100 w-40 h-28 rounded-lg drop-shadow-md opacity-60'></div>
+                      <div className='absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-center z-0  group-hover:z-[11]'>
+                        <svg
+                          className='mx-auto h-12 w-12 text-base-content opacity-100'
+                          stroke='currentColor'
+                          fill='none'
+                          viewBox='0 0 48 48'
+                          aria-hidden='true'
+                        >
+                          <path
+                            d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
+                            strokeWidth={2}
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                          />
+                        </svg>
+                        <div className='flex text-sm text-base-content'>
+                          <label
+                            htmlFor='vidFileUpload'
+                            className='badge cursor-pointer text-center'
+                          >
+                            <span className='mx-auto'>Upload a file</span>
+                            <input
+                              id='vidFileUpload'
+                              name='vidFileUpload'
+                              type='file'
+                              className='sr-only'
+                              onChange={onChangeVideo}
+                            />
+                          </label>
+                        </div>
+                        <p className='text-xs text-base-content'>
+                          mp4,avi,etc up to 10MB
                         </p>
                       </div>
                     </div>
