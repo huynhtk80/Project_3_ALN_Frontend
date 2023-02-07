d374126dda32ca8204c04961a4c48c5abc0c20e9
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2023-02-07 00:01:48 -0700

    basic chat functionality

---
+++ src/components/Chat.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { FirebaseContext } from '../providers/FirebaseProvider';
+import { AuthContext } from '../providers/AuthProvider';
+
+const Chat = () => {
+  const { user } = useContext(AuthContext);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await firebase.firestore().collection('messages').add({
+      text: newMessage,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setNewMessage('');
+  };
+
+  return user ? (
+    <div
+      className={`fixed bottom-0 right-0 ${
+        isOpen ? 'w-72' : 'w-72 h-0 pb-10'
+      } bg-primary-focus text-base-content p-5 m-2 border border-primary rounded-lg shadow-lg bg-opacity-80 transition-all duration-300`}
+    >
+      <header className=' flex flex-row justify-between'>
+        <h2 className='text-md text-primary-content'>ALN Chat</h2>
+        <button className='btn btn-xs btn-square' onClick={handleToggle}>
+          {isOpen ? 'X' : '-'}
+        </button>
+      </header>
+      {isOpen && (
+        <>
+          <div className='h-64 w-96 overflow-y-scroll'>
+            <ul>
+              {messages.map((message) => (
+                <li key={message.id} className='text-sm pb-2'>
+                  {message.text}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <form onSubmit={handleSubmit} className='pt-4'>
+            <input
+              className='w-full p-2 border border-gray-300 rounded-lg'
+              type='text'
+              value={newMessage}
+              onChange={(event) => setNewMessage(event.target.value)}
+            />
+            <button
+              className='w-full mt-2 p-2 bg-gray-400 text-white rounded-lg hover:bg-gray-500'
+              type='submit'
+              onClick={handleSubmit}
+            >
+              Send
+            </button>
+          </form>
+        </>
+      )}
+    </div>
+  ) : null;
+};
+
+export default Chat;
