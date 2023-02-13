6d1643202290cab2624054e267cc75c0377d1f5c
Timothy H <huynh.tk80@gmail.com>
2023-02-12 18:25:21 -0700

    VideoDetails credits and creator

--- src/pages/UploadVideo.tsx
+++ src/pages/UploadVideo.tsx
@@ -1,15 +1,8 @@
-import React, { useContext, useState } from 'react';
-import { FirebaseContext } from '../providers/FirebaseProvider';
-import { AuthContext } from '../providers/AuthProvider';
-import { addDoc, collection, updateDoc } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import ReactPlayer from 'react-player';
-import { uploadFileStorage } from '../utils/fireStorageAPI';
+import { useContext } from 'react';
 import ListUserMovies from '../components/ListUserMovies';
 import SelectAndUpFile from '../components/SelectAndUpFile';
-import { addMovie } from '../utils/fireStoreAPI';
-import UploadedVidDetail from '../components/UploadedVidDetail';
-import AdminApproveC from '../components/AdminApproveC';
+import { AuthContext } from '../providers/AuthProvider';
+import { FirebaseContext } from '../providers/FirebaseProvider';
 
 function UploadVideo() {
   const fbContext = useContext(FirebaseContext);
