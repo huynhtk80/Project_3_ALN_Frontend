b2db058cd2f311adf11348258e35b4c220e5b106
Timothy H <huynh.tk80@gmail.com>
2023-02-15 21:55:46 -0700

    info styled

--- src/components/LandingInfoSimple.tsx
+++ src/components/LandingInfoSimple.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
+import { HiOutlineLightBulb } from 'react-icons/hi';
+import { FaGlobeAfrica, FaRegMoneyBillAlt } from 'react-icons/fa';
 
 function LandingInfoSimple() {
   return (
-    <div className='w-full h-60 flex flex-row justify-between items-center gap-3 bg-white'>
+    <div className='w-full h-fit flex flex-row justify-between items-center gap-3 bg-white my-5'>
       <div className='flex flex-col justify-between items-center gap-5 p-5'>
-        <div>Icon lightbulb</div>
-        <h2 className='text-xl'>The African Story</h2>
+        <div className='text-orange-600'>
+          <HiOutlineLightBulb size={'60px'} />
+        </div>
+        <h2 className='text-2xl font-bold text-center'>The African Story</h2>
         <div className=' text-center'>
           This platform gives you a glimpse of Africa in detail, its various
           distinctions its diversity, richness, value, uniqueness, and wealth.
         </div>
       </div>
-      <div className='flex flex-col justify-between items-center p-5'>
-        <div>Icon globe</div>
-        <div>Continental Reach</div>
+      <div className='flex flex-col justify-between items-center gap-5 p-5'>
+        <div className='text-orange-600'>
+          <FaGlobeAfrica size={'60px'} />
+        </div>
+        <h2 className='text-2xl font-bold text-center'>Continental Reach</h2>
         <div className=' text-center'>
           New documentaries and films are being made in various cities in
           America, the United Kingdom, Australia, and Asia.
         </div>
       </div>
-      <div className='flex flex-col justify-between items-center p-5'>
-        <div>Icon moneybill</div>
-        <div>invest in Africa</div>
+      <div className='flex flex-col justify-between items-center gap-5 p-5'>
+        <div className='text-orange-600'>
+          <FaRegMoneyBillAlt size={'60px'} />
+        </div>
+        <h2 className='text-2xl font-bold text-center'>invest in Africa</h2>
         <div className=' text-center'>
           We offer exclusive access to valuable, rich content from Africa that
           is guaranteed to bring investors profit due to its lucrative model.
