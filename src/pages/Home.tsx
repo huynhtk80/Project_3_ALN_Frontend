1ce3a490b35b697436626f3582191347bb224f61
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-02-17 16:14:36 -0700

    Possible News section, to be discussed with Jack.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,15 @@ function Home() {
       <div className=' pt-20 pb-15 mx-7 ' style={{ overflow: 'hidden' }}>
         <AfricanMap />
       </div>
+
+      {/* <div className=' pt-15 pb-15 mx-56 '>
+        <iframe
+          src='//www.africanews.com/embed/timeline'
+          scrolling='no'
+          frameBorder='0'
+          style={{ minHeight: '1000px', width: '100%', height: '100%' }}
+        ></iframe>
+      </div> */}
     </>
   );
 }
