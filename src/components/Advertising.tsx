38261d348cb61488246b12c3b370774d0d024c91
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-02-20 02:23:49 -0700

    added rough draft of advertising tab

--- src/components/Advertising.tsx
+++ src/components/Advertising.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Advertising() {
-  return <div className='pt-20'>Advertising</div>;
+  return (
+    <div className='pt-20'>
+      <h1>Advertising</h1>
+      You can advertise with us through our advertising platform on the channel.
+      ALN will provide opportunities for banners in different parts of the
+      platform and on pages with a lot of traffic.It will also provide for
+      advertisements through brand placement and free subscription accounts that
+      will allow for advertisement on breaks. Companies will be given an
+      opportunity to place their adverts on selected available criteria.
+    </div>
+  );
 }
 
 export default Advertising;
