9519fc53b15bcf2aa59740f04007e8bdca508317
Timothy H <huynh.tk80@gmail.com>
2023-02-20 23:36:18 -0700

    updated Pricing Tiers

--- src/components/PricingTiers.tsx
+++ src/components/PricingTiers.tsx
@@ -4,32 +4,47 @@ function PricingTiers() {
       <div className='flex flex-col items-center my-5'>
         <h1 className='text-4xl mb-5'>Choose Your Plan</h1>
         <p className='mb-6'>Choose a plan that works best for you.</p>
-        <div className='flex flex-col  sm:flex-row justify-center items-center gap-4'>
+        <div className='flex flex-col  md:flex-row justify-center items-center gap-4'>
           {[
             {
-              plan: 'Beta Trial',
-              cost: 'free',
-              features: ['preliminary access', 'connection with network'],
+              plan: 'Entertainment, first month free!',
+              cost: '$4.99',
+              features: [
+                'Watch unlimited videos',
+                'Setup profile and share your preferred content',
+                'Share links of content with friends and family',
+                'Watch highlights and trailers of all new content',
+                'Access to film competitions and quizzes',
+              ],
               disabled: false,
             },
             {
-              plan: 'Starter',
-              cost: 'tbd',
-              features: ['preliminary access', 'connection with network'],
+              plan: 'Networker, Coming soon!',
+              cost: '$8.99',
+              features: [
+                'All Entertainment Features',
+                'Connect with other networkers, Entreprenuers and businesses',
+                'Have chat capability with the networker community',
+                'Limited access to live streaming, webinars and product launches.',
+              ],
               disabled: true,
             },
             {
-              plan: 'Premium',
-              cost: 'tbd',
-              features: ['preliminary access', 'connection with network'],
+              plan: 'Entrepreneur, Coming soon!',
+              cost: '$11.99',
+              features: [
+                'All Networker Features',
+                'Access to ALN the business directory',
+                'Access to virtual business symposiums, conferences and fairs',
+              ],
               disabled: true,
             },
           ].map((card, index) => (
             <div
               key={index + card.plan}
-              className='card max-w-96 bg-primary-focus shadow-xl hover:scale-105 transition-all duration-300 ease-in-out'
+              className='card w-96 min-h-[28rem] bg-primary-focus shadow-xl hover:scale-105 transition-all duration-300 ease-in-out'
             >
-              <div className='card-body items-center gap-6'>
+              <div className='card-body items-center justify-between h-full '>
                 <h2 className='card-title'>{card.plan}</h2>
                 <h2 className='card-title'>
                   <span className='text-4xl'>{card.cost}</span>/month
