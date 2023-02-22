55eefb271044cf3a614d9f6611b4b58301d49093
Jennifer Reid <113141579+Juniper007@users.noreply.github.com>
2023-02-22 14:38:38 -0700

    Styled Marketing / Services offered

--- src/pages/Marketing.tsx
+++ src/pages/Marketing.tsx
@@ -4,10 +4,13 @@ function Marketing() {
       <h1 className='text-3xl font-bold mb-10 pt-20 text-center'>
         Services Offered by Africa Live Network
       </h1>
-      <div className='grid justify-items-start container mx-auto px-4 mb-10'>
-        <div className='italic font-bold'></div>
-        <br /> <br />
-        <br /> <br />
+      <div className='italic font-bold grid justify-items-center'>
+        Have a great idea and want to get it started? Let us know and we can
+        help.
+      </div>
+      <div className='flex flex-wrap gap-8 justify-center my-8'>
+        {/* <div className='grid justify-items-evenly auto-rows-auto container mx-auto px-4 mb-10'> */}
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -30,19 +33,15 @@ function Marketing() {
               dreams.
             </p>
             <div className='card-actions justify-end'>
-              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-sm mt-5'>
+                <a href='https://jacksshackint.com/' target='_blank'>
+                  Jack's Shack
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <br /> <br />
-        <div className='italic font-bold'>
-          Want to create a film, have it produced and a place to share it?
-        </div>{' '}
-        <br />
-        Our team can help with creating new productions from scratch. <br />
-        Starting with conceptualization, scripting, filming, post production and
-        packaging of projects.
-        <br /> <br />
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -51,32 +50,25 @@ function Marketing() {
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title'>Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className='card-title'>
+              Want to create a film, have it produced and a place to share it?
+            </h2>
+            <p>
+              Our team can help with creating new productions from scratch.{' '}
+              <br />
+              Starting with conceptualization, scripting, filming, post
+              production and packaging of projects.
+            </p>
             <div className='card-actions justify-end'>
-              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-sm mt-5'>
+                <a href='https://jacksshackint.com/' target='_blank'>
+                  Jack's Shack
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <br /> <br />
-        <div className='italic font-bold'>
-          Are you looking to sponsor or be sponsored?
-        </div>{' '}
-        <br />
-        Connect with us to discuss our sponsorship program where we connect
-        investors who want to be apart of the production fun by supporting our
-        various entrepreneurs.
-        <br /> Helping produce talk shows, tv series, podcasts, movies,
-        documentaries, and a variety of other media channels that wish to
-        promote African culture, history, innovation, and success stories
-        illuminating the best of the diaspora and native people along with
-        supporting growing African businesses.
-        <br />
-        <div className='italic font-bold'>
-          Have a great idea and want to get it started? Let us know and we can
-          help.
-        </div>
-        <br /> <br />
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -85,25 +77,29 @@ function Marketing() {
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title'>Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className='card-title'>
+              Are you looking to sponsor or be sponsored?
+            </h2>
+            <p>
+              Connect with us to discuss our sponsorship program where we
+              connect investors who want to be apart of the production fun by
+              supporting our various entrepreneurs.
+              <br /> Helping produce talk shows, tv series, podcasts, movies,
+              documentaries, and a variety of other media channels that wish to
+              promote African culture, history, innovation, and success stories
+              illuminating the best of the diaspora and native people along with
+              supporting growing African businesses.
+            </p>
             <div className='card-actions justify-end'>
-              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-sm mt-5'>
+                <a href='https://jacksshackint.com/' target='_blank'>
+                  Jack's Shack
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <br /> <br />
-        <div className='italic font-bold'>
-          Need assistance finding & securing filming locations in Africa?{' '}
-        </div>{' '}
-        <br />
-        Through our locations management services, we help film makers who would
-        like to shoot productions in any part of the continent that interests
-        them by building the necessary data and research so that by the time the
-        film crew arrives, there will be all the necessary resources available
-        for a full production set to proceed. Done by utilizing Africa Live
-        Network's Baobab Community.
-        <br /> <br />
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -112,25 +108,27 @@ function Marketing() {
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title'>Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className='card-title'>
+              Need assistance finding & securing filming locations in Africa?
+            </h2>
+            <p>
+              Through our locations management services, we help film makers who
+              would like to shoot productions in any part of the continent that
+              interests them by building the necessary data and research so that
+              by the time the film crew arrives, there will be all the necessary
+              resources available for a full production set to proceed. Done by
+              utilizing Africa Live Network's Baobab Community.
+            </p>
             <div className='card-actions justify-end'>
-              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-sm mt-5'>
+                <a href='https://jacksshackint.com/' target='_blank'>
+                  Jack's Shack
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <br /> <br />
-        <div className='italic font-bold'>
-          Something in your area that celebrates African culture in your area
-          that the world should see?
-        </div>{' '}
-        <br />
-        Long term goals are to work with government officials all across the
-        continent in stimulating economic growth by promoting tourism through
-        high quality videos that highlight the different beauties in and around
-        Africa. Highlighting cultural cuisine, areas to visit, monuments, and
-        heritage sites.
-        <br /> <br />
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -139,22 +137,27 @@ function Marketing() {
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title'>Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className='card-title'>
+              Something in your area that celebrates African culture in your
+              area that the world should see?
+            </h2>
+            <p>
+              Long term goals are to work with government officials all across
+              the continent in stimulating economic growth by promoting tourism
+              through high quality videos that highlight the different beauties
+              in and around Africa. Highlighting cultural cuisine, areas to
+              visit, monuments, and heritage sites.
+            </p>
             <div className='card-actions justify-end'>
-              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-sm mt-5'>
+                <a href='https://jacksshackint.com/' target='_blank'>
+                  Jack's Shack
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <br /> <br />
-        <div className='italic font-bold'>
-          Interested in Traveling to Africa and Would Like Personal
-          Recommendations From Someone Who Knows The Area?
-        </div>
-        <br />
-        Africa Live Network has contacts with travel agents and boards that will
-        make travel much easier and more accessible from any part of the world.
-        <br /> <br />
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -163,23 +166,25 @@ function Marketing() {
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title'>Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className='card-title'>
+              Interested in Traveling to Africa and Would Like Personal
+              Recommendations From Someone Who Knows The Area?
+            </h2>
+            <p>
+              Africa Live Network has contacts with travel agents and boards
+              that will make travel much easier and more accessible from any
+              part of the world.
+            </p>
             <div className='card-actions justify-end'>
-              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-sm mt-5'>
+                <a href='https://jacksshackint.com/' target='_blank'>
+                  Jack's Shack
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <br /> <br />
-        <div className='italic font-bold'>
-          Ready to Revolutionize the World with a New Product, But Need Help
-          Launching?
-        </div>{' '}
-        <br />
-        Through various supporting entities and partnerships, Africa Live
-        Network offers Unique Solutions for Product Launch Support in Africa,
-        the United States of America, Canada, & around the world.
-        <br /> <br />
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -188,18 +193,25 @@ function Marketing() {
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title'>Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className='card-title'>
+              Ready to Revolutionize the World with a New Product, But Need Help
+              Launching?
+            </h2>
+            <p>
+              Through various supporting entities and partnerships, Africa Live
+              Network offers Unique Solutions for Product Launch Support in
+              Africa, the United States of America, Canada, & around the world.
+            </p>
             <div className='card-actions justify-end'>
-              <button className='btn btn-primary'>Buy Now</button>
+              <button className='btn btn-sm mt-5'>
+                <a href='https://jacksshackint.com/' target='_blank'>
+                  Jack's Shack
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <br /> <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        {/* <br /> */}
         <div className='card w-96 bg-base-100 shadow-xl image-full'>
           <figure>
             <img
@@ -226,7 +238,7 @@ function Marketing() {
             <div className='card-actions justify-end'>
               <button className='btn btn-sm mt-5'>
                 <a href='https://jacksshackint.com/' target='_blank'>
-                  Jacks Shack
+                  Jack's Shack
                 </a>
               </button>
             </div>
